430679265c2a86c63ce1ac1246775030c30ca7e9
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-04-25 11:57:17 +0200

    adjust
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- bin/cyclonedx-npm-cli.js
+++ bin/cyclonedx-npm-cli.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
-require('../dist/cli.js')
-  .run(process)
-  .catch(e => {
-    process.stderr.write(`\n${e}\n`)
-    return Math.max(1, Math.floor(Number(e?.code)) || 254)
-  })
-  .then(exitCode => {
-    process.exitCode = exitCode
-  })
+require('../dist/cli.js').run(process).catch(e => {
+  process.stderr.write(`\n${e}\n`)
+  return Math.max(1, Math.floor(Number(e?.code)) || 254)
+}).then(exitCode => {
+  process.exitCode = exitCode
+})
