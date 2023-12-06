b5af86bc09f17b99d44a3ab85baec662b052d0b9
jase88 <804836+jase88@users.noreply.github.com>
2023-12-06 20:44:42 +0100

    fix: writing to stderr
    Signed-off-by: jase88 <804836+jase88@users.noreply.github.com>

--- src/logger.ts
+++ src/logger.ts
@@ -17,15 +17,11 @@ SPDX-License-Identifier: Apache-2.0
 Copyright (c) OWASP Foundation. All Rights Reserved.
 */
 
-import { stderr } from 'node:process'
-
 import { type BaseLogger, type Level, type LevelWithSilent, type LoggerExtras, pino } from 'pino'
 
 const logLevels: Level[] = ['debug', 'error', 'fatal', 'warn', 'info', 'trace'] as const
 export const verbosityLevels: LevelWithSilent[] = [...logLevels, 'silent'] as const
 
-// all output shall be bound to stdError - stdOut is for result output only
-const streams = logLevels.map((level) => ({ level, stream: stderr }))
 
 export type Logger = BaseLogger & LoggerExtras<{
   level: LevelWithSilent
@@ -42,9 +38,11 @@ export const createLogger: (verbosityLevel: VerbosityLevel) => Logger = (verbosi
     target: 'pino-pretty',
     options: {
       colorize: true,
-      ignore: 'pid,name,hostname'
+      translateTime: "SYS:yyyy-mm-dd HH:MM:ss.l",
+      ignore: 'pid,name,hostname',
+      destination: 2
     }
   }
-}, pino.multistream(streams))
+})
 
 export const defaultLogLevel: VerbosityLevel = 'warn'
