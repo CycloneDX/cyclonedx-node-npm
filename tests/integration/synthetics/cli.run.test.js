1bebed4079c011b3a79df057c4186b4807b6c1b3
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-06-02 21:30:54 +0200

    dings
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/integration/synthetics/cli.run.test.js
+++ tests/integration/synthetics/cli.run.test.js
@@ -217,7 +217,7 @@ describe('cli.run()', () => {
         })
 
         try {
-          expect(res).resolves.toBe(0)
+          await expect(res).resolves.toBe(0)
         } catch (err) {
           process.stderr.write(readFileSync(errFile))
           throw err
@@ -271,7 +271,7 @@ describe('cli.run()', () => {
     })
 
     try {
-      expect(res).resolves.toBe(0)
+      await expect(res).resolves.toBe(0)
     } catch (err) {
       process.stderr.write(readFileSync(errFile))
       throw err
@@ -333,7 +333,7 @@ describe('cli.run()', () => {
       })
 
       try {
-        expect(res).resolves.toBe(0)
+        await expect(res).resolves.toBe(0)
       } catch (err) {
         process.stderr.write(readFileSync(errFile))
         throw err
