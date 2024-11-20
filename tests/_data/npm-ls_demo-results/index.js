e5ad764cedd03bf5f065c849bbd198564e9eeb07
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-11-20 11:27:02 +0100

    tests: less unnessessarry tests
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/_data/npm-ls_demo-results/index.js
+++ tests/_data/npm-ls_demo-results/index.js
@@ -20,14 +20,24 @@ Copyright (c) OWASP Foundation. All Rights Reserved.
 const { sync: glob } = require('fast-glob')
 
 const fileGlob = '*/CI_results/*.json'
+
 const filePattern = /\/(?<subject>[^/]+?)\/CI_results\/npm-ls(?<args>.*?)_npm(?<npm>.+?)_node(?<node>.+?)_(?<os>.+?).json$/i
+/** @typedef fileMatch
+ * @prop {string} path
+ * @prop {string} subject
+ * @prop {string} args
+ * @prop {string} npm
+ * @prop {string} node
+ * @prop {string} os
+ */
+
 /** @type {import('fast-glob').OptionsInternal} */
 const globOptions = { absolute: true, caseSensitiveMatch: false, cwd: __dirname, deep: 3, onlyFiles: true, unique: true }
 
 let cached
 
 /**
- * @return {{path:string, subject?:string, args?:string, npm?:string, node?:string, os?:string}[]}
+ * @return {Array<fileMatch>}
  */
 function index () {
   if (cached === undefined) {
