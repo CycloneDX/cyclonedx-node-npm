8f15f337790b386cdde5766dac3e83190cd9be40
Jan Kowalleck <jan.kowalleck@gmail.com>
2026-02-24 13:32:47 +0100

    docs
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- src/_helpers.ts
+++ src/_helpers.ts
@@ -132,9 +132,13 @@ export function normalizePackageManifest (data: any): asserts data is normalizeP
 
 /**
  * Ignore pattern for `resolved`.
- * - ignore: well, just ignore it ... i guess.
- * - file: local dist cannot be shipped and therefore should be ignored.
+ * - `^ignore:`- well, just ignore it ... i guess.
+ * - `^file:`  - local dist cannot be shipped and therefore should be ignored.
  */
 export const npmResolvedIgnoreMatcher = /^(?:ignore|file):/i
 
-export const npmResolvedVcsMatcher = /^git(?:\+[^:]+)?:/i
+/**
+ * VCS pattern for `resolved`.
+ * - `^git:` & `^git+...:` & `.git$`- git
+ */
+export const npmResolvedVcsMatcher = /^git(?:\+[^:]+)?:|.git$/i
