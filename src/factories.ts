ba44b86a61699df19d25604387750e7eeaf2d933
Jan Kowalleck <jan.kowalleck@gmail.com>
2026-02-25 12:51:51 +0100

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- src/factories.ts
+++ src/factories.ts
@@ -93,10 +93,10 @@ export class PackageUrlFactory {
     // docs: https://blog.npmjs.org/post/172999548390/new-pgp-machinery
     /* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- acknowledged */
     const { tarball } = packageJson.dist ?? {}
-    if ( isString(tarball) && tarball.length > 5
-      && !FromNodePackageJsonUtils.defaultRegistryMatcher.test(tarball)
-    ) {
-      qualifiers[PurlQualifierNames.DownloadUrl] = tarball
+    if ( isString(tarball) && tarball.length > 5 ) {
+      if (!FromNodePackageJsonUtils.defaultRegistryMatcher.test(tarball)) {
+        qualifiers[PurlQualifierNames.DownloadUrl] = tarball
+      }
     } else if ( typeof packageJson.repository === 'object' ) {
       try {
         const url = new URL(packageJson.repository.url)
@@ -106,7 +106,7 @@ export class PackageUrlFactory {
           url.hash = subdir
         }
         qualifiers[PurlQualifierNames.VcsUrl] = url.toString()
-      } catch (e) {
+      } catch {
         /* pass */
       }
     }
